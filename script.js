c8c9d462b96faa1de86f70f944e974b9b86d29b2
Jmara <jmara.0923@gmail.com>
2024-09-01 01:53:55 +0200

    Add functions to each box

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
+const defaultGridSize = 16;
 let button = document.querySelector(".btn");
+let container = document.querySelector(".container");
 
-function addGrid() {
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.toggle("row");
+    container.appendChild(newRow);
+    for (let j = 0; j < size; j++) {
+      const newDiv = document.createElement("div");
+      newDiv.classList.toggle("grid");
+      newRow.appendChild(newDiv);
+    }
+  }
+}
+
+createGrid(defaultGridSize);
+
+function changeGrid() {
   let size = parseInt(prompt("How many grid?"));
   if (size > 100) {
     return alert("Please put numbers less than 100");
   }
-
-  let createDiv = document.createElement("Div");
-  let container = document.querySelector(".container");
-  createDiv.classList.toggle("grid");
-  container.appendChild(createDiv);
+  container.replaceChildren();
+  createGrid(size);
 }
 
-button.addEventListener("click", addGrid);
+function changeBG(e) {
+  return div.classList.toggle("hover");
+}
+
+button.addEventListener("click", changeGrid);
+
+const divs = document.querySelectorAll(".grid");
+divs.forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    const box = e.target;
+    box.style.cssText = "background-color: black";
+  });
+});
+
+// container.addEventListener("mouseenter", (e) => {
+//   console.log(e);
+//   const box = e.target;
+//   box.style.cssText = "background-color: black";
+// });
